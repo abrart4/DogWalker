628e0fd1550955323edc47c3ef7c1a647e79ab2c
AbrarTaseen <abrart4@nycstudents.net>
2025-09-15 11:43:09 -0400

    Base class

---
+++ src/DogWalkCompany.java
@@ -0,0 +1,20 @@
+public class DogWalkCompany {
+    /**
+     * Returns the number of dogs, always greater than 0, that are available
+     * for a walk during the time specified by hour
+     * Precondition: 0 <= hour <= 23
+     */
+    public int numAvailableDogs(int hour) {
+
+    }
+
+    /**
+     * Decreases, by numberDogsWalked, the number of dogs available for a walk
+     * during the time specified by hour
+     * Preconditions: 0 <= hour <= 23
+     *                numberDogsWalked > 0
+     */
+    public void updateDogs(int hour, int numberDogsWalked) {
+
+    }
+}
