0e5a5b458869300cf0c5695a6aae7609c225682a
AbrarTaseen <abrart4@nycstudents.net>
2025-10-10 10:52:42 -0400

    Added AP test cases

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
+        /* appears at the beginning, scroll up in terminal */
+        System.out.println("----- AP test case -----");
+        DogWalkCompany testCompany = new DogWalkCompany();
+        testCompany.setNumAvaliableDogs(7, 3);
+        testCompany.setNumAvaliableDogs(8, 2);
+        testCompany.setNumAvaliableDogs(9, 2);
+        testCompany.setNumAvaliableDogs(10, 3);
+        DogWalker testWalker = new DogWalker(3, testCompany);
+        int testAmountForHour7 = testWalker.dogWalkShift(7, 7);
+        int testAmountForHour8 = testWalker.dogWalkShift(8, 8);
+        int testAmountForHour9 = testWalker.dogWalkShift(9, 9);
+        int testAmountForHour10 = testWalker.dogWalkShift(10, 10);
+        int testAmountTotal = testAmountForHour7 + testAmountForHour8 + testAmountForHour9 + testAmountForHour10;
+        System.out.println("- Max dogs of walker: 3");
+        System.out.println("- Dogs available:");
+        System.out.println("  - Hour 7: 3");
+        System.out.println("  - Hour 8: 2");
+        System.out.println("  - Hour 9: 2");
+        System.out.println("  - Hour 10: 3");
+        System.out.println("* Amount earned:");
+        System.out.println("  * Hour 7: $" + testAmountForHour7);
+        System.out.println("  * Hour 8: $" + testAmountForHour8);
+        System.out.println("  * Hour 9: $" + testAmountForHour9);
+        System.out.println("  * Hour 10: $" + testAmountForHour10);
+        System.out.println("  ** Total: $" + testAmountTotal);
+        System.out.println("------------------");
+
         DogWalkCompany c = new DogWalkCompany();
         System.out.println(c);
         int hour = c.addDogs();
